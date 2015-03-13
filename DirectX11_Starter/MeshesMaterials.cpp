b32379a8604c176dfe5da6a8cfe80c015cc6af1c
unknown <twb1193@GDD-21.main.ad.rit.edu>
2015-03-13 09:19:16 -0400

    Revert "Revert "Pulled out Meshes/Materials and Samplers into their own classes. Currently the menu screen draws but the "game" screen is just cornflower blue. Need to figure out what is broken.""
    This reverts commit d00ddf26f5d7b6c0b48675d1ec79264bcdb60c69.

---
+++ DirectX11_Starter/MeshesMaterials.cpp
@@ -0,0 +1,75 @@
+#include "MeshesMaterials.h"
+
+Mesh* MeshesMaterials::triangleMesh = NULL;
+Mesh* MeshesMaterials::quadMesh = NULL;
+Mesh* MeshesMaterials::cubeMesh = NULL;
+Mesh* MeshesMaterials::frameMesh = NULL;
+Mesh* MeshesMaterials::environmentMesh = NULL;
+Mesh* MeshesMaterials::particleMesh = NULL;
+
+Material* MeshesMaterials::shapeMaterial = NULL;
+Material* MeshesMaterials::buttonMaterial = NULL;
+Material* MeshesMaterials::titleMaterial = NULL;
+Material* MeshesMaterials::labelMaterial = NULL;
+Material* MeshesMaterials::frameMaterial = NULL;
+Material* MeshesMaterials::tileMaterial = NULL;
+Material* MeshesMaterials::particleMaterial = NULL;
+
+MeshesMaterials::MeshesMaterials()
+{
+}
+
+MeshesMaterials::~MeshesMaterials()
+{
+}
+
+void MeshesMaterials::Destructor()
+{
+	// Clean up meshes
+	delete triangleMesh;
+	delete quadMesh;
+	delete cubeMesh;
+	delete frameMesh;
+	delete environmentMesh;
+	delete particleMesh;
+
+	// Clean up materials
+	delete shapeMaterial;
+	delete buttonMaterial;
+	delete titleMaterial;
+	delete labelMaterial;
+	delete frameMaterial;
+	delete tileMaterial;
+	delete particleMaterial;
+}
+
+// Load each of the game's meshes and materials
+void MeshesMaterials::LoadMeshesAndMaterials(ID3D11Device* device, ID3D11DeviceContext* deviceContext)
+{
+	// Prepare some variables
+	ObjLoader loader = ObjLoader();
+	ID3D11Buffer* vertexBuffer;
+	ID3D11Buffer* indexBuffer;
+	int size;
+
+	// Create materials
+	shapeMaterial = new Material(device, deviceContext, Shaders::vertexShader, Shaders::pixelShader, Samplers::linearSampler, L"image.png");
+	buttonMaterial = new Material(device, deviceContext, Shaders::vertexShader, Shaders::pixelShader, Samplers::linearSampler, L"button.png");
+	titleMaterial = new Material(device, deviceContext, Shaders::vertexShader, Shaders::pixelShader, Samplers::linearSampler, L"title.png");
+	labelMaterial = new Material(device, deviceContext, Shaders::vertexShader, Shaders::pixelShader, Samplers::linearSampler, L"label.png");
+	frameMaterial = new Material(device, deviceContext, Shaders::vertexShader, Shaders::pixelShader, Samplers::linearSampler, L"texFrame.png");
+	tileMaterial = new Material(device, deviceContext, Shaders::vertexShader, Shaders::pixelShader, Samplers::anisotropicSampler, L"tile.png");
+	particleMaterial = new Material(device, deviceContext, Shaders::particleVertexShader, Shaders::particlePixelShader, Samplers::linearSampler, L"texLBlock.png", Shaders::particleGeometryShader);
+
+	// Load meshes
+	size = loader.Load("cube.txt", device, &vertexBuffer, &indexBuffer);
+	cubeMesh = new Mesh(device, deviceContext, vertexBuffer, indexBuffer, size);
+	size = loader.Load("frame.txt", device, &vertexBuffer, &indexBuffer);
+	frameMesh = new Mesh(device, deviceContext, vertexBuffer, indexBuffer, size);
+	size = loader.Load("environment.txt", device, &vertexBuffer, &indexBuffer);
+	environmentMesh = new Mesh(device, deviceContext, vertexBuffer, indexBuffer, size);
+
+	// Create 2D meshes
+	triangleMesh = new Mesh(device, deviceContext, TRIANGLE);
+	quadMesh = new Mesh(device, deviceContext, QUAD);
+}
