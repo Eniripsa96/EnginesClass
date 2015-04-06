be3656497377eb41cf693a269c79e7812a17e0d1
Thomas Bentley <twb1193@rit.edu>
2015-04-06 16:15:44 -0400

    Added more notes. Got rid of extra code. Particle system will now have a new mesh each time it is reset, acting like the particle system generates a random particle configuration each time.

--- DirectX11_Starter/ParticleSystem.cpp
+++ DirectX11_Starter/ParticleSystem.cpp
@@ -8,17 +8,13 @@ ParticleSystem::ParticleSystem(Mesh* mesh, Material* mat)
 
 	age = 0.0f;
 
-	XMFLOAT3 particles[3] =
-	{
-		{ XMFLOAT3(+0.0f, +0.5f, +0.0f) },
-		{ XMFLOAT3(-1.0f, -0.5f, +0.0f) },
-		{ XMFLOAT3(+1.0f, -0.5f, +0.0f) },
-	};
-
 	XMStoreFloat4x4(&world, (XMMatrixTranslation(0.0f, 0.0f, 0.0f)));
 
 	// Need to make the vertex buffer a dynamic buffer 
 		// Necessary because the particles will be updated every frame if we want them to move 'realistically'
+	// Need to use StreamOut to update the particle system
+		// Physics calcs and logic to update particles will go in shader
+		// Right now physics is in this class using a cbuffer
 }
 
 
@@ -30,6 +26,12 @@ ParticleSystem::~ParticleSystem()
 // Restart the particle system
 void ParticleSystem::Reset()
 {
+	// Temporary
+	ID3D11Device* dev = mesh->device;
+	ID3D11DeviceContext* devC = mesh->deviceContext;
+	delete mesh;
+	mesh = new Mesh(dev, devC, PARTICLE);
+
 	age = INITIAL_AGE;
 	velocity = INITIAL_VEL;
 	XMStoreFloat4x4(&world, (XMMatrixTranslation(0.0f, 0.0f, 0.0f)));
