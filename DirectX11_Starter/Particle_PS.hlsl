e81679842e64ce0ecc73cb8355c77e82a14c4305
unknown <twb1193@GDD-23.main.ad.rit.edu>
2015-04-28 10:13:21 -0400

    Cleanup.

--- DirectX11_Starter/Particle_PS.hlsl
+++ DirectX11_Starter/Particle_PS.hlsl
@@ -1,12 +1,8 @@
-//Texture2D myTexture : register(t0);
-//SamplerState mySampler: register(s0);
-
 // Defines the input to this pixel shader
 // - Should match the output of our corresponding vertex shader
 struct PSInput
 {
 	float4 position		: SV_POSITION;
-	//float4 initialPos	: VELOCITY;
 	float2 uv			: TEXCOORD0;
 	float3 color		: COLOR;
 };
@@ -14,7 +10,6 @@ struct PSInput
 // Entry point for this pixel shader
 float4 main(PSInput input) : SV_TARGET
 {
-	// Return color sampled from texture
-	//return myTexture.Sample(mySampler, input.uv);
+	// Return the color
 	return float4(input.color, 1.0f);
 }
