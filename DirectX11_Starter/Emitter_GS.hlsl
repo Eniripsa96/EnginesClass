8c7eb39f57db397a47a19fa9518e6f4f334fdd9d
unknown <twb1193@GDD-23.main.ad.rit.edu>
2015-04-30 10:34:28 -0400

    Started using the randomized 1D texture to randomize velocity in the emitter geometry shader. Having issues with sampling, right now there are only 4 velocities that are repeated over and over again within the 50 particles.

--- DirectX11_Starter/Emitter_GS.hlsl
+++ DirectX11_Starter/Emitter_GS.hlsl
@@ -13,18 +13,24 @@ struct VertexOutput
 void main(point VertexOutput input[1] /*: SV_POSITION*/, inout PointStream<VertexOutput> output)
 {
 	VertexOutput vert;
-	float2 uv = float2(10.0f, 10.0f);
-	float4 v = randomTex.SampleLevel(mySampler, uv, 0);
+	//float2 uv = float2(age.y * 1000.0f, (1.0f - age.y) * 1000.0f);
+	//float4 v = randomTex.SampleLevel(mySampler, uv, 0);
+	//float2 uv;
 
 	[unroll]
-	for (int i = 0; i < 3; i++)
+	for (int i = 0; i < 50; i++)
 	{
 		vert.position = float4(0.0f, 0.0f, 0.0f, 1.0f);
-		vert.initialVel = input[0].initialVel;
+		
+		//uv = float2(i * 1000.f, i + 2);
+		//vert.initialVel = input[0].initialVel;
 		//vert.initialVel = float4(v.x, v.y, v.z, 1.0f);
+		vert.initialVel = randomTex.SampleLevel(mySampler, i / 500.0f, 0);
+		vert.initialVel.w = 1.0f;
+
 		vert.size = input[0].size;
-		//vert.color = input[0].color;
-		vert.color = float4(v);
+		vert.color = input[0].color;
+		//vert.color = float4(v);
 
 		output.Append(vert);
 	}
