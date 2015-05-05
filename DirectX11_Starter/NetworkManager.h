4451f301dda137095b752a5404d640fc040675f2
Steven Sucy <sms6462@g.rit.edu>
2015-05-04 21:46:40 -0400

    Hid away methods that shouldn't be called elsewhere and added comments

--- DirectX11_Starter/NetworkManager.h
+++ DirectX11_Starter/NetworkManager.h
@@ -25,22 +25,50 @@ class NetworkManager
 public:
 	NetworkManager();
 	~NetworkManager();
+	
+	// Checks whether or not the network is connected to another user
 	bool isConnected();
+
+	// Checks whether or not the network data is set up and ready to be used
 	bool isSetUp();
+
+	// Starts listening to a hosting user at the given IP/Port combination.
+	// If the IP and Port are not provided, this will listen on local host.
+	// Returns whether or not the connection was successful.
 	bool startListening(PCSTR ip = DEFAULT_IP, PCSTR port = DEFAULT_PORT);
+
+	// Starts a host setup to listen for other joining users
+	// Returns whether or not the setup was successful.
 	bool startServer();
-	bool tryHost();
-	bool tryConnect(PCSTR ip = DEFAULT_IP, PCSTR port = DEFAULT_PORT);
+
+	// Disconnects the user from it's current network communications.
 	void disconnect();
+
+	// Emits data over the network to other users.
+	// If not currently connected, this will do nothing.
 	bool emit(packetStruct* packet);
+
+	// Checks whether or not the network has received any data.
 	bool hasData();
+
+	// Retrieves the next packet received from the network in the queue to process.
 	packet getData();
 
 	SOCKET Socket;
 	bool connected;
 	bool setUp;
 	std::queue<packet> received;
+
 private:
+	
+	// Attempts to set up a host setup for the network connections.
+	// Returns whether or not it was successful.
+	bool tryHost();
+
+	// Attempts to connect to a hosting user at the given IP/Port combination.
+	// Returns whether or not the connection was successful.
+	bool tryConnect(PCSTR ip = DEFAULT_IP, PCSTR port = DEFAULT_PORT);
+
 	WSADATA wsaData;
 	std::thread* listenThread;
 };
