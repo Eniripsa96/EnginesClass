a47ced1db5c405f5123b5afde10f8ecad7552da7
unknown <twb1193@NM2550-4.main.ad.rit.edu>
2015-05-16 20:14:06 -0400

    Judges now take into account all 3 components of their favorite color when scoring a particle effect.

--- DirectX11_Starter/Judge.cpp
+++ DirectX11_Starter/Judge.cpp
@@ -23,9 +23,20 @@ Judge::~Judge()
 
 void Judge::JudgeEffect(XMFLOAT3* color)
 {
+	// Create factor to put scores between 0-10
 	float maxScoreFactor = 10.0f / 127.5f;	// Max score over max color value
-	int score = (10 - (abs(color->x*255 - favColor.x) * maxScoreFactor)) + 0.5f;
-	score = score < 0 ? 0 : score;
+
+	// Calculate component scores based on favorite color
+	int redScore = (10 - (abs(color->x*255 - favColor.x) * maxScoreFactor)) + 0.5f;
+	int greenScore = (10 - (abs(color->y * 255 - favColor.y) * maxScoreFactor)) + 0.5f;
+	int blueScore = (10 - (abs(color->z * 255 - favColor.z) * maxScoreFactor)) + 0.5f;
+	// Fix the edge-case where scores are -2 which is an invalid score
+	redScore = redScore < 0 ? 0 : redScore;
+	greenScore = greenScore < 0 ? 0 : greenScore;
+	blueScore = blueScore < 0 ? 0 : blueScore;
+
+	// Compile the color scores into an overall score
+	int score = (redScore + greenScore + blueScore) / 3.0f + 0.5f;
 
 	if (score == 0)
 		material = MeshesMaterials::materials["judge0"];
